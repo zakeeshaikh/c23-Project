ef22d5dd19263f30ffd0e560614b5e18b3c126af
Zakee Shaikh <78408518+zakeeshaikh@users.noreply.github.com>
2021-05-17 19:51:40 +0530

    Add files via upload

---
+++ package.js
@@ -0,0 +1,37 @@
+class Package {
+ 
+    constructor(x,y,widht,height){
+    
+    this.body = Bodies.rectangle(x,y,width,height);
+    this.widht = width;
+    this.height = height;
+    
+    World.add(world,this.body);
+    }
+    display(){
+    
+        var pos =this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x,pos.y)
+        rotate(angle);
+        rectMode(CENTER);
+        fill(255);
+        rect(0,0, this.width, this.height);
+        pop();
+      }
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
